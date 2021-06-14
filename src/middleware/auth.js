4f174838ee9b890e77a36064fa1024c74bee67e2
pinaki_prasanna_das <61472356+pinakiprasannadas2000@users.noreply.github.com>
2021-06-14 19:36:38 +0530

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,7 +4,7 @@ const User = require('../models/user')
 const auth = async (req, res, next) => {
   try {
     const authToken = req.header('Authorization').replace('Bearer ', '')
-    const decodedData = jwt.verify(authToken, 'MangekyoSharingan')
+    const decodedData = jwt.verify(authToken, 'jgijeij83a3j933ar3tai9ti9a938*U*TU*#*UR#UO3uuruq3rjqiojr93u9rurq3')
     const user = await User.findOne({
       _id: decodedData._id,
       'authTokens.authToken': authToken,
